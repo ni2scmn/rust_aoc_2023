05ade197d52eefb3e0302bad9f93daaabf1f28d4
Nils Schnakenberg <nils.schnakenberg@gmx.net>
2023-12-02 14:02:17 +0100

    day2 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,30 +1,78 @@
+use std::f32::INFINITY;
+
 #[derive(Debug)]
-struct Game {
+struct CubeSet {
     green: i32,
     blue: i32,
     red: i32,
 }
 
-impl Game {
+impl CubeSet {
     const MAX_RED: i32 = 12;
     const MAX_GREEN: i32 = 13;
     const MAX_BLUE: i32 = 14;
 
+    pub fn is_valid(&self) -> bool {
+        if self.blue > CubeSet::MAX_BLUE || self.green > CubeSet::MAX_GREEN || self.red > CubeSet::MAX_RED {
+            return false;
+        }
+        true
+    }
+
+    pub fn cube_power(&self) -> i32 {
+        self.red * self.blue * self.green
+    }
+}
+
+#[derive(Debug)]
+struct Game {
+    cubes: CubeSet,
+}
+
+impl Game {
     pub fn new() -> Game {
         Game {
-            blue: 0,
-            green: 0,
-            red: 0,
+            cubes: CubeSet {
+                green: 0,
+                red: 0,
+                blue: 0,
+            },
+        }
+    }
+}
+
+struct GameSet {
+    games: Vec<Game>
+}
+
+impl GameSet {
+    pub fn new() -> GameSet {
+        GameSet {
+            games: Vec::new()
         }
     }
 
     pub fn is_valid(&self) -> bool {
-        if self.blue > Game::MAX_BLUE ||
-            self.green > Game::MAX_GREEN ||
-            self.red > Game::MAX_RED {
-            return false;
-        }
-        true
+        self.games.iter().fold(true, |acc, x| acc && x.cubes.is_valid())
+    }
+
+    pub fn minimal_game(&self) -> Game {
+        let mut min_game = Game::new();
+
+        self.games.iter().for_each(|g| {
+
+            if g.cubes.red > min_game.cubes.red {
+                min_game.cubes.red = g.cubes.red
+            }
+            if g.cubes.blue > min_game.cubes.blue {
+                min_game.cubes.blue = g.cubes.blue
+            }
+            if g.cubes.green > min_game.cubes.green {
+                min_game.cubes.green = g.cubes.green
+            }
+        });
+
+        min_game
     }
 }
 
@@ -34,11 +82,14 @@ pub fn day_2_1() {
         .collect::<Vec<_>>();
 
     let mut valid_id_sum: i32 = 0;
+    let mut min_cube_power_sum: i32 = 0;
 
     for l in input {
+
         let (game_id_str, draws_str) = l.split_once(':').unwrap();
         let game_id = game_id_str.replace("Game ", "").parse::<i32>().unwrap();
-        let mut game_is_valid = true;
+
+        let mut game_set = GameSet::new();
 
         draws_str.split(';').for_each(|d_str| {
             let mut game = Game::new();
@@ -50,24 +101,26 @@ pub fn day_2_1() {
                 let q = q_str.parse::<i32>().unwrap();
 
                 match c {
-                    "red" => game.red = q,
-                    "green" => game.green = q,
-                    "blue" => game.blue = q,
+                    "red" => game.cubes.red = q,
+                    "green" => game.cubes.green = q,
+                    "blue" => game.cubes.blue = q,
                     _ => {
                         panic!("Unexpected")
                     }
                 }
             });
 
-            if !game.is_valid() {
-                game_is_valid = false
-            }
+            game_set.games.push(game);
+
         });
 
-        if game_is_valid {
+        if game_set.is_valid() {
             valid_id_sum += game_id;
         }
+
+        min_cube_power_sum += game_set.minimal_game().cubes.cube_power();
     }
 
     println!("Day 2 Part 1 answer: {}", valid_id_sum);
+    println!("Day 2 Part 1 answer: {}", min_cube_power_sum);
 }
