a1cdb4246e97a48a608b83224aba1a08ed46f167
Nils Schnakenberg <nils.schnakenberg@gmx.net>
2023-12-04 10:10:17 +0100

    day 4 part 1

---
+++ src/day4.rs
@@ -0,0 +1,40 @@
+pub fn day4_1() {
+    let input: Vec<&str> = include_str!("../input/day_4_1_input.txt")
+        .split('\n')
+        .collect::<Vec<_>>();
+
+    let mut winning_sum = 0;
+    
+    for l in input {
+        let (_game_id, game) = l.split_once(':').unwrap();
+        
+        let (winning_cards_str, drawn_cards_str) = game.split_once('|').unwrap();
+
+        let winning_cards: Vec<_> = winning_cards_str.split(' ').collect::<Vec<&str>>().iter().map(|x| {
+            x.parse::<i32>()
+        }).filter(|x| x.is_ok()).map(|x| x.unwrap()).collect();
+
+        let drawn_cards: Vec<_> = drawn_cards_str.split(' ').collect::<Vec<&str>>().iter().map(|x| {
+            x.parse::<i32>()
+        }).filter(|x| x.is_ok()).map(|x| x.unwrap()).collect();
+
+        let mut round_sum = 0;
+
+        winning_cards.iter().for_each(|x| {
+
+            for dc in drawn_cards.iter() {
+                if *x == *dc {
+                    if round_sum == 0 {
+                        round_sum = 1;
+                    } else {
+                        round_sum *= 2;
+                    }
+                    break;
+                }
+            }
+        });
+        winning_sum += round_sum;
+    }
+    
+    println!("Day 4 Part 1 answer: {}", winning_sum);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use rust_2023::day1::{day_1_1, day_1_2};
 use rust_2023::day2::day_2_1;
 use rust_2023::day3::day_3_1;
+use rust_2023::day4::day4_1;
 
 fn main_run() {
     day_1_1();
     day_1_2();
     day_2_1();
     day_3_1();
+    day4_1();
 }
 
 fn main() {
