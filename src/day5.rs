98e940f2d5be0e526289216ec6a8ab84fdda85b8
Nils Schnakenberg <nils.schnakenberg@gmx.net>
2023-12-05 19:35:20 +0100

    day 5 part 1

---
+++ src/day5.rs
@@ -0,0 +1,89 @@
+use std::collections::HashMap;
+
+fn parse_seed(l: &str) -> Vec<i64> {
+    l.replace("seeds: ", "")
+        .split(' ')
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect()
+}
+
+pub fn day5_1() {
+    let input: Vec<&str> = include_str!("../input/day_5_1_input.txt")
+        .split('\n')
+        .collect::<Vec<_>>();
+
+    let mut maps: Vec<Vec<(i64, i64, i64)>> = Vec::new(); 
+
+    let mut cur_map: Option<Vec<(i64, i64, i64)>> = None;
+
+    let mut seeds: Option<Vec<i64>> = None;
+    for l in input {
+        println!("inputline: {}", l);
+
+        if l.contains("seeds") {
+            seeds = Some(parse_seed(l));
+        } else if l.contains("map") {
+            if let Some(cm) = cur_map {
+                maps.push(cm);
+            }
+            cur_map = Some(Vec::new());
+        } else if l.chars().count() == 0 {
+            continue;
+        } else {
+            let map_rule = l
+                .split(' ')
+                .map(|x| x.parse::<i64>().unwrap())
+                .collect::<Vec<_>>();
+
+            let destination_start = map_rule.get(0).unwrap();
+            let source_start = map_rule.get(1).unwrap();
+            let range_length = map_rule.get(2).unwrap();
+
+            // println!("destination_start {}", destination_start);
+            // println!("source_start {}", source_start);
+            // println!("range_length {}", range_length);
+
+            let shift = destination_start - source_start;
+            let source_end = source_start + range_length;
+
+
+            cur_map.as_mut().unwrap().push((*source_start, source_end, shift));
+
+            // println!("maprule: {:?}", map_rule);
+            // println!("current map rules{:?}", cur_map.as_ref().unwrap());
+        }
+    }
+    if let Some(cm) = cur_map {
+        maps.push(cm);
+    }
+
+    println!("seeds {:?}", seeds.as_ref().unwrap());
+
+    let mut minimum = i64::MAX; 
+
+    for s in seeds.as_ref().unwrap() {
+
+        let mut seed = *s;
+
+        for (idx, m) in maps.iter().enumerate() {
+            println!("map: {:?}", m);
+
+            println!("pre_map {}: {}", idx, seed);
+
+            for mr in m {
+                if seed >= mr.0 && seed < mr.1 {
+                    seed += mr.2;
+                    break;
+                }
+            }
+
+            println!("post_map {}: {}", idx, seed);
+        }
+
+        if seed < minimum {
+            minimum = seed;
+        }
+    }
+
+    println!("Day 5 Part 1 answer: {}", minimum);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use rust_2023::day1::{day_1_1, day_1_2};
 use rust_2023::day2::day_2_1;
 use rust_2023::day3::day_3_1;
 use rust_2023::day4::{day4_1, day4_2};
+use rust_2023::day5::day5_1;
 
 fn main_run() {
     day_1_1();
@@ -10,6 +11,7 @@ fn main_run() {
     day_3_1();
     day4_1();
     day4_2();
+    day5_1();
 }
 
 fn main() {
