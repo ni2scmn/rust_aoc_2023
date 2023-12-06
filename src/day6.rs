2b14f0498eb9e16545f23eaf40746d801523d384
Nils Schnakenberg <nils.schnakenberg@gmx.net>
2023-12-06 09:12:35 +0100

    day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -1,47 +1,61 @@
-pub fn day_6_1() {
-
-  let input: Vec<&str> = include_str!("../input/day_6_1_input.txt")
+fn day_6_calc(ip: &str) -> i64 {
+    let input: Vec<&str> = ip
         .split('\n')
         .collect::<Vec<_>>();
 
-  let times_str = input.first().unwrap().replace("Time: ", "");
-  let times: Vec<i32> = times_str.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect();
-  let distances_str = input.get(1).unwrap().replace("Distance: ", "");
-  let distances: Vec<i32> = distances_str.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect();
+    let times_str = input.first().unwrap().replace("Time: ", "");
+    let times: Vec<i64> = times_str
+        .split_whitespace()
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect();
+    let distances_str = input.get(1).unwrap().replace("Distance: ", "");
+    let distances: Vec<i64> = distances_str
+        .split_whitespace()
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect();
 
-  if times.len() != distances.len() {
-      panic!("Unmatching times and distances size");
-  }
+    if times.len() != distances.len() {
+        panic!("Unmatching times and distances size");
+    }
 
-  let mut winning_possibilities: Vec<i32> = Vec::new();
+    let mut winning_possibilities: Vec<i64> = Vec::new();
 
-  for x in 0..times.len() {
+    for x in 0..times.len() {
+        let mut round_winning_possibilities = 0;
 
-      let mut round_winning_possibilities = 0;
+        let t = times.get(x).unwrap();
+        let d = distances.get(x).unwrap();
 
-      let t = times.get(x).unwrap();
-      let d = distances.get(x).unwrap();
+        // println!("t: {}, d: {}", t, d);
 
-      println!("t: {}, d: {}", t, d);
+        for y in 0..*t {
+            let s = y;
+            let dt = s * (*t - y);
 
-      for y in 0..*t {
-          let s = y;
-          let dt = s * (*t - y);
+            // println!("s: {}, dt: {}", s, dt);
 
-          println!("s: {}, dt: {}", s, dt);
+            if dt > *d {
+                // println!("Round won");
+                round_winning_possibilities += 1;
+            } else {
+                // println!("Round lost");
+            }
+        }
 
-          if dt > *d {
-              println!("Round won");
-              round_winning_possibilities += 1;
-          } else {
-              println!("Round lost");
-          }
-      } 
+        winning_possibilities.push(round_winning_possibilities);
+    }
 
+    return winning_possibilities.iter().product::<i64>();
+}
 
-      winning_possibilities.push(round_winning_possibilities);
+pub fn day_6_1() {
+    println!(
+        "Day 6 Part 1 answer: {:?}",
+        day_6_calc(include_str!("../input/day_6_1_input.txt"))
+    );
 
-  }
-
-  println!("Day 6 Part 1 answer: {:?}", winning_possibilities.iter().product::<i32>());
+    println!(
+        "Day 6 Part 2 answer: {:?}",
+        day_6_calc(include_str!("../input/day_6_2_input.txt"))
+    );
 }
